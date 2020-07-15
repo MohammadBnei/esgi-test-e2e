1440d60f7a666508a95027245bd1ddbdb44c0d83
MohammadBnei <mohammad-amine.banaei@gmail.com>
2020-07-15 11:37:08 +0200

    Update

--- test/test.js
+++ test/test.js
@@ -41,84 +41,84 @@ var firefoxBrowser = new webdriver.Builder()
 
 const cssLocator = webdriver.By.css
 
-for (const browser of [chromeBrowser, firefoxBrowser]) {
-	browser.manage().window().maximize();
-	describe("Home Page", function() {
+const browsers = [chromeBrowser, firefoxBrowser]
 
-		before(function () {
-			return browser.get(serverUri)
-				.then(() => browser.wait(webdriver.until.elementLocated(webdriver.By.css('main'), 5000)))
-		});
 
-		after(function() {
-			return browser.quit();
-		});
-
-		/**
-		 * Test case to load our application and check the title.
-		 */
-		it("Should load the home page and get title", async function() {
-				const title = await browser.getTitle()
-				assert.strictEqual(title, appTitle);
-		});
-		
-		it("Should load the articles", async function() {
-				const articlesCount = await browser.findElements(cssLocator('li'))
-				assert.strictEqual(articlesCount.length, 6)
-		});
-
-		it("Should add an article to the cart", async function() {
-			const firstArticle = await browser.findElement(cssLocator('.product-price'))
-			let articlePrice = await firstArticle.findElement(cssLocator('div'))		
-			const articleBtn = await firstArticle.findElement(cssLocator('button'))
-			await articleBtn.click()
-
-			await browser.wait(webdriver.until.elementLocated(cssLocator('div.total>div'), 2000))
-
-			const total = await browser.findElement(cssLocator('div.total>div'))
-
-			
-			let text = await total.getText()
-			articlePrice = await articlePrice.getText()
-			
-			
-			text = text.replace('Total: ', '')
-			
-			should.exist(total)
-			text.should.equal(articlePrice)
-		});
-
-		it("Should proceed to checkout", async function() {
-			const total = await browser.findElement(cssLocator('div.total'))
-			const proceedBtn = await total.findElement(cssLocator('button'))
-
-			await proceedBtn.click()
-		});
-
-		it("Should checkout", async function() {
-			
-			const email = await browser.findElement(webdriver.By.name('email'))
-			const name = await browser.findElement(webdriver.By.name('name'))
-			const address = await browser.findElement(webdriver.By.name('address'))
-
-			await browser.wait(webdriver.until.elementLocated(webdriver.By.id('checkoutBtn'), 1500))
-
-			const checkoutBtn = await browser.findElement(webdriver.By.id('checkoutBtn'))
-
-			await Promise.all([email.sendKeys('test@test.com'),
-			name.sendKeys('Hello testing'),
-			address.sendKeys('12 rue de l\'école')])
-
-			await checkoutBtn.click()
-
-			await browser.wait(webdriver.until.elementLocated(cssLocator('div.main'), 2000))
-
-			let success = await browser.findElement(cssLocator('div.main'))
-			success = await success.getText()
-
-			assert.equal(success, "Payment Validated !")
-
-			await browser.sleep(5000)
-		});
+const testSuite = async browser => describe("Home Page", function() {	
+	before(function () {
+		browser.manage().window().maximize();
+		return browser.get(serverUri)
+			.then(() => browser.wait(webdriver.until.elementLocated(webdriver.By.css('main'), 5000)))
 	});
-}
+
+	after(function() {
+		return browser.quit();
+	});
+
+	/**
+	 * Test case to load our application and check the title.
+	 */
+	it("Should load the home page and get title", async function() {
+			const title = await browser.getTitle()
+			assert.strictEqual(title, appTitle);
+	});
+	
+	it("Should load the articles", async function() {
+			const articlesCount = await browser.findElements(cssLocator('li'))
+			assert.strictEqual(articlesCount.length, 6)
+	});
+
+	it("Should add an article to the cart", async function() {
+		const firstArticle = await browser.findElement(cssLocator('.product-price'))
+		let articlePrice = await firstArticle.findElement(cssLocator('div'))		
+		const articleBtn = await firstArticle.findElement(cssLocator('button'))
+		await articleBtn.click()
+
+		await browser.wait(webdriver.until.elementLocated(cssLocator('div.total>div'), 2000))
+
+		const total = await browser.findElement(cssLocator('div.total>div'))
+
+		
+		let text = await total.getText()
+		articlePrice = await articlePrice.getText()
+		
+		
+		text = text.replace('Total: ', '')
+		
+		should.exist(total)
+		text.should.equal(articlePrice)
+	});
+
+	it("Should proceed to checkout", async function() {
+		const total = await browser.findElement(cssLocator('div.total'))
+		const proceedBtn = await total.findElement(cssLocator('button'))
+
+		await proceedBtn.click()
+	});
+
+	it("Should checkout", async function() {
+		
+		const email = await browser.findElement(webdriver.By.name('email'))
+		const name = await browser.findElement(webdriver.By.name('name'))
+		const address = await browser.findElement(webdriver.By.name('address'))
+
+		await browser.wait(webdriver.until.elementLocated(webdriver.By.id('checkoutBtn'), 1500))
+
+		const checkoutBtn = await browser.findElement(webdriver.By.id('checkoutBtn'))
+
+		await Promise.all([email.sendKeys('test@test.com'),
+		name.sendKeys('Hello testing'),
+		address.sendKeys('12 rue de l\'école')])
+
+		await checkoutBtn.click()
+
+		await browser.wait(webdriver.until.elementLocated(cssLocator('div.main'), 2000))
+
+		let success = await browser.findElement(cssLocator('div.main'))
+		success = await success.getText()
+
+		assert.equal(success, "Payment Validated !")
+	});
+});
+
+browsers.map(testSuite)
